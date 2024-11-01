5488a033a72ba940f5afd21e3988c215dc99affe
Piyush Borakhade <pborakhade7@gmail.com>
2024-11-01 22:50:40 +0530

    Update eval_plots.py

--- eval_plots.py
+++ eval_plots.py
@@ -1,1 +1,21 @@
+import matplotlib.pyplot as plt
 
+def evaluationPlotAccuracy(model):
+    plt.plot(model.history["accuracy"], color="blue", label="accuracy")
+    plt.plot(model.history["val_accuracy"], color="red", label="val_accuracy")
+    plt.title("Training And Validation Accuracy Score")
+    plt.xlabel("accuracy")
+    plt.ylabel("epochs")
+    plt.grid()
+    plt.legend()
+    plt.show()
+
+def evaluationPlotLoss(model):
+    plt.plot(model.history["loss"], color="blue", label="loss")
+    plt.plot(model.history["val_loss"], color="red", label="val_loss")
+    plt.title("Training And Validation Loss Score")
+    plt.xlabel("loss")
+    plt.ylabel("epochs")
+    plt.grid()
+    plt.legend()
+    plt.show()
